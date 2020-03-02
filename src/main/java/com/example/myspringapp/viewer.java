69032e8fa52f25b9d5ada8a33eb7652aa0a012ef
munashek92 <munashek92@gmail.com>
2020-03-02 11:58:45 +0200

    I have added a java bean

---
+++ src/main/java/com/example/myspringapp/viewer.java
@@ -0,0 +1,23 @@
+package com.example.myspringapp;
+
+public class viewer {
+    private String name;
+    private int ID;
+
+    public viewer(){
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public int getID(){
+        return this.ID;
+    }
+    public void setID(int ID){
+        this.ID=ID;
+    }
+}
